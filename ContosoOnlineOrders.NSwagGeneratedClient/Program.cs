d23c398fa829b6b1f00e08ace93d1a1d89df04d2
bradygaster <bradyg@microsoft.com>
2021-01-31 23:38:55 -0800

    updated client sdk with operationId

--- ContosoOnlineOrders.NSwagGeneratedClient/Program.cs
+++ ContosoOnlineOrders.NSwagGeneratedClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 
 namespace ContosoOnlineOrders.NSwagGeneratedClient
@@ -10,31 +11,116 @@ namespace ContosoOnlineOrders.NSwagGeneratedClient
             using(var httpClient = new HttpClient())
             {
                 var apiClient = new ContosoOnlineOrders_ApiClient(
-                    "https://localhost:5001",
+                    "http://localhost:5000",
                     httpClient
                 );
 
-                /*
-                ------------------------------------------------------------
-                Without explicit operationId: Note how the generated SDK
-                code for the API isn't discoverable. Each method is named
-                specific to the 
-                ------------------------------------------------------------
-                apiClient.CheckInventoryAsync(1);                           // check a product's inventory
-                apiClient.CheckInventoryAsync(Guid.Empty)                   // check an order's inventory
-                apiClient.CheckInventory2Async(1,                           // update a product's inventory
+#if OperationId
+                /*------------------------------------------------------------
+                With explicit operationId: Note how the API advertises itself
+                and is easy to use and discover.
+                ------------------------------------------------------------*/
+
+                // create a product
+                apiClient.CreateProductAsync(new CreateProductRequest
+                {
+                    Id = 1000,
+                    InventoryCount = 0,
+                    Name = "Test Product"
+                });
+
+                // update a product's inventory
+                apiClient.UpdateProductInventoryAsync(1,
                     new InventoryUpdateRequest
                     {
-                        CountToAdd = 5, ProductId = 1
+                        CountToAdd = 50,
+                        ProductId = 1000
                     });
-                apiClient.Orders2Async(Guid.Empty);                         // get one order
-                apiClient.OrdersAllAsync();                                 // get all orders
-                apiClient.OrdersAsync(new Order());                         // create a new order
-                apiClient.Products2Async(1);                                // get one product
-                apiClient.ProductsAllAsync(new CreateProductRequest());     // create a product
-                apiClient.ProductsAsync();                                  // get all products
-                apiClient.ShipAsync(Guid.Empty);                            // ship an order
-                */
+
+                // get all products
+                apiClient.GetProductsAsync();
+
+                // get one product
+                apiClient.GetProductAsync(1000);
+
+                // create a new order
+                Guid orderId = Guid.NewGuid();
+
+                apiClient.CreateOrderAsync(new Order
+                {
+                    Id = orderId,
+                    Items = new CartItem[]
+                    {
+                        new CartItem { ProductId = 1000, Quantity = 10 }
+                    }
+                });
+
+                // get one order
+                apiClient.GetOrderAsync(orderId);
+
+                // get all orders
+                apiClient.GetOrdersAsync();
+
+                // check an order's inventory
+                apiClient.CheckInventoryAsync(orderId);
+
+                // ship an order
+                apiClient.ShipOrderAsync(orderId);
+#else
+                /*------------------------------------------------------------
+                Without explicit operationId: Note how the generated SDK
+                code for the API isn't discoverable.
+
+                If you were discovering the API's shape using IntelliSense alone,
+                you'd have to look at the parameters to know how to call the API.
+                ------------------------------------------------------------*/
+
+                // create a product
+                apiClient.ProductsAllAsync(new CreateProductRequest
+                {
+                    Id = 1000,
+                    InventoryCount = 0,
+                    Name = "Test Product"
+                });
+
+                // update a product's inventory
+                apiClient.CheckInventory2Async(1,                           
+                new InventoryUpdateRequest
+                    {
+                        CountToAdd = 50,
+                        ProductId = 1000
+                    });
+
+                // get all products
+                apiClient.ProductsAsync();
+
+                // get one product
+                apiClient.Products2Async(1000);
+
+                // create a new order
+                Guid orderId = Guid.NewGuid();
+
+                apiClient.OrdersAsync(new Order
+                {
+                    Id = orderId,
+                    Items = new CartItem[]
+                    {
+                        new CartItem { ProductId = 1000, Quantity = 10 }
+                    }
+                });
+
+                // get one order
+                apiClient.Orders2Async(orderId);
+
+                // get all orders
+                apiClient.OrdersAllAsync();
+
+                // check an order's inventory
+                apiClient.CheckInventoryAsync(orderId);
+
+                // ship an order
+                apiClient.ShipAsync(orderId);
+#endif
             }
 
             Console.WriteLine("Hello World!");
