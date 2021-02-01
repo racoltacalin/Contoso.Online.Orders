4903cb068a876ec4603f76d1cbb2cbc2ae4c6b9d
bradygaster <bradyg@microsoft.com>
2021-01-31 21:12:59 -0800

    settable property for IsShipped

--- ContosoOnlineOrders.Api/Models/Order.cs
+++ ContosoOnlineOrders.Api/Models/Order.cs
@@ -3,5 +3,8 @@ using System.Collections.Generic;
 
 namespace ContosoOnlineOrders.Api.Models
 {
-    public record Order(Guid Id, List<CartItem> Items);
+    public record Order(Guid Id, List<CartItem> Items)
+    {
+        public bool IsShipped { get; set; } = false;
+    }
 }
